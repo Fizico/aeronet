4fb8b37e19e3074aea35b2fc3425cad07ccb7a7d
Brav-Aranda, J.A <jbravo@lmd.polytechnique.fr>
2016-11-01 11:41:43 +0100

    Title included to the function aeronet_plot_ONEILL_box.

--- aeronet_plot_lev.m
+++ aeronet_plot_lev.m
@@ -15,8 +15,7 @@ print([out '.png'],'-dpng');
 % eval(['!mogrify -trim ' [out '.png']]); %trim image edges using ImageMagick
 
 clf;
-aeronet_plot_ONEILL_box(aero.jd(mask), aero.angstrom(mask,3), 'AE_{\alpha})(440-675nm)',...
-                        [7:11], [1:6,12] )
+aeronet_plot_ONEILL_box(aero.jd(mask), aero.angstrom(mask,3), 'AE_{\alpha})(440-675nm)', 7:11, [1:6,12], 0, sprintf('AE_{\\alpha}(440-675nm) Wishker box: %s-%s', datestr(aero.jd(1),'mmm/yy'), datestr(aero.jd(end),'mmm/yy')));
 out=[aero.file '_angstrom_box.png'];
 print(out,'-dpng'); eval(['!mogrify -trim ' out]);
 
@@ -28,7 +27,7 @@ print(out,'-dpng'); eval(['!mogrify -trim ' out]);
 
 clf;
 aeronet_plot_ONEILL_box(aero.jd(mask), aero.water(mask,1), 'Water (cm)',...
-                        [7:11], [1:6,12] )
+                        7:11, [1:6,12] )
 out=[aero.file '_water_box.png'];
 print(out,'-dpng'); eval(['!mogrify -trim ' out]);
 %
